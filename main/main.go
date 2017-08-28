d1ee1c35e412e7aca92d5add738c6d9767f83030
islam ezzat <islam3zzat@gmail.com>
2017-08-28 21:50:18 +0200

    using our newtain util

--- main/main.go
+++ main/main.go
@@ -6,3 +6,18 @@ import (
 	"fmt"
 	"../utils"
 )
+
+func main() {
+	if argsCount := len(os.Args); argsCount > 1 {
+		value, err := strconv.ParseFloat(os.Args[1], 64)
+		if err != nil {
+			fmt.Println("Couldn't parse provided value")
+		} else {
+			fmt.Println(utils.NeotainSqrt(value))
+		}
+	} else {
+		fmt.Println("please enter a value")
+	}
+
+
+}
